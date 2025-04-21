efbef7d3cfa36bbba71151bc5be8e7264d694948
Jasser Alshaer <joshaer17@gmail.com>
2025-04-21 21:49:00 +0300

    Add project files.

---
+++ Jordana/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+using Jordana.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jordana.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+    }
+}
