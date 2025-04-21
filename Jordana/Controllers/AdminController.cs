449146e763c97df16eb1f9afba893364b0df7cf2
Rahaf Ibrahim <2021980162@ses.yu.edu.jo>
2025-04-21 22:15:39 +0300

    I created Admin functionality

---
+++ Jordana/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Jordana.Controllers
+{
+    public class AdminController : Controller 
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult ManageDestinations()
+        {
+            return View();
+        }
+
+        public IActionResult ManageBooking()
+        {
+            return View();
+        }
+
+        public IActionResult ManageUsers()
+        {
+            return View();
+        }
+
+        public IActionResult ManageRating()
+        {
+            return View();
+        }
+    }
+}
