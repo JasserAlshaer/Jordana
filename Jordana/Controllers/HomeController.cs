09bd42789196b01d10dca77a00dfd26f825861d2
Roula <m7md@DESKTOP-SOBVJDT>
2025-04-24 21:39:55 +0300

    I created Home Function

--- Jordana/Controllers/HomeController.cs
+++ Jordana/Controllers/HomeController.cs
@@ -6,6 +6,40 @@ namespace Jordana.Controllers
 {
     public class HomeController : Controller
     {
-        
+
+        public IActionResult service()
+        {
+
+            return View();
+        }
+
+        public IActionResult Feature()
+        {
+
+            return View();
+        }
+
+
+        public IActionResult Testimoniala()
+        {
+
+            return View();
+        }
+
+
+
+
+        public IActionResult StaticInformation()
+        {
+
+            return View();
+        }
+
+
+        public IActionResult Facts()
+        {
+
+            return View();
+        }
     }
 }
